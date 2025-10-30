13a5becd23e9eef92ca7068fc30db7c6944c9fab
rahulkotian <rahulkotian98@gmail.com>
2025-10-30 12:54:16 +0530

    Update Firebase configuration for GitHub Pages deployment

--- js/firebase.js
+++ js/firebase.js
@@ -34,8 +34,9 @@ import {
 const firebaseConfig = {
   apiKey: "AIzaSyC4pZOIZikLKjL_eYAW9x3aC4weSz9PP6I",
   authDomain: "monez-a4619.firebaseapp.com",
+  databaseURL: "https://monez-a4619-default-rtdb.firebaseio.com",
   projectId: "monez-a4619",
-  storageBucket: "monez-a4619.firebasestorage.app",
+  storageBucket: "monez-a4619.appspot.com",
   messagingSenderId: "754794079933",
   appId: "1:754794079933:web:fb794db5e96a94dd9f39d4",
   measurementId: "G-LTF8XKRR6P"
